2623277fc9a100b89165f43cb5a34fea5a8e2959
Carlos Godoy trapero <carlos.godoy.trapero@ericsson.com>
2024-12-10 11:12:43 +0100

    Latest version of generic

--- exercises/generic.go
+++ exercises/generic.go
@@ -1,11 +1,80 @@
 package exercises
 
 func AnyOf[T any](items []T, criteria func(T) bool) bool {
-
 	for _, item := range items {
 		if criteria(item) {
 			return true
 		}
 	}
 	return false
+}
+
+func FindIf[T any](items []T, criteria func(T) bool) int {
+	for index, item := range items {
+		if criteria(item) {
+			return index
+		}
+	}
+	return -1
+}
+
+func AdjacentFind[T any](items []T, criteria func(T, T) bool) int {
+	for index, item := range items {
+		if index+1 >= len(items) {
+			return -1
+		}
+		if criteria(item, items[index+1]) {
+			return index
+		}
+	}
+	return -1
+}
+
+func Equal[T comparable](left []T, right []T) bool {
+	if (left == nil) != (right == nil) {
+		return false
+	}
+
+	if len(left) != len(right) {
+		return false
+	}
+
+	for index, item := range left {
+		if item != right[index] {
+			return false
+		}
+	}
+	return true
+}
+
+func ReplaceIf[T comparable](items []T, element T, criteria func(T) bool) int {
+	if items == nil {
+		return -1
+	}
+
+	for index, item := range items {
+		if criteria(item) {
+			items[index] = element
+			return index
+		}
+	}
+
+	return -1
+}
+
+func RemoveIf[T any](items *[]T, criteria func(T) bool) bool {
+	if items == nil {
+		return false
+	}
+
+	tmp := make([]T, len(*items))
+	i := 0
+	for _, item := range *items {
+		if criteria(item) == true {
+			tmp[i] = item
+			i++
+		}
+	}
+	items = &tmp
+	return true
 }
--- exercises/generic_test.go
+++ exercises/generic_test.go
@@ -11,9 +11,76 @@ func TestGivenAnyOfWhenIntegerSimpleComparisonThenTrue(t *testing.T) {
 	}
 }
 
-func TestGivenAnyOfWhenSpringComparisonThenTrue(t *testing.T) {
+func TestGivenAnyOfWhenStringComparisonThenTrue(t *testing.T) {
 	if AnyOf([]string{"foo", "bar", "baz"},
 		func(i string) bool { return i == "bar" }) == false {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenFindIfWhenFoundStringComparisonThenOne(t *testing.T) {
+	if FindIf([]string{"foo", "bar", "baz"},
+		func(i string) bool { return i == "bar" }) != 1 {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenAdjacentFindWhenNotFoundStringComparisonThenMinusOne(t *testing.T) {
+	if AdjacentFind([]string{"foo", "bar", "baz"},
+		func(i string, j string) bool { return i == j }) != -1 {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenAdjacentFindWhenFoundStringComparisonThenOne(t *testing.T) {
+	if AdjacentFind([]string{"foo", "bar", "bar"},
+		func(i string, j string) bool { return i == j }) != 1 {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenEqualWhenBothAreEqualThenTrue(t *testing.T) {
+	if Equal([]string{"foo", "bar", "bar"},
+		[]string{"foo", "bar", "bar"}) != true {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenEqualWhenBothAreNotEqualThenTrue(t *testing.T) {
+	if Equal([]string{"foo", "bar", "bar"},
+		[]string{"foo", "baz", "bar"}) != false {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenEqualWhenOneAreNilThenFalse(t *testing.T) {
+	if Equal([]string{"foo", "bar", "bar"},
+		nil) != false {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenReplaceIflWhenComplainsAndExistsThenOne(t *testing.T) {
+	origin := []string{"foo", "bar", "bar"}
+	if ReplaceIf(origin,
+		"carlos",
+		func(i string) bool { return i == "bar" }) != 1 {
+		t.Errorf("No works")
+	}
+
+	if origin[1] != "carlos" {
+		t.Errorf("No works")
+	}
+}
+
+func TestGivenRemoveIflWhenComplainsAndExistsThenOne(t *testing.T) {
+	origin := []string{"foo", "bar", "baz"}
+	if !RemoveIf(&origin,
+		func(i string) bool { return i != "bar" }) {
+		t.Errorf("No works")
+	}
+
+	if len(origin) != 1 {
 		t.Errorf("No works")
 	}
 }
